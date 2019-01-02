4ab82a225be404007adec7ef8f341cc24236c15c
AlexCovizzi <alexcovizzi@gmail.com>
2019-01-02 11:44:29 +0100

    spostato constraints in una classe, implementato Standard Backtracking

--- main.py
+++ main.py
@@ -2,13 +2,11 @@ from typing import Callable, Dict, Tuple, List
 
 
 class CSPSolver:
-
-
     # init
-    def __init__(self, algorithm: Callable[[CSPNode, int], bool] = CSPAlgorithm.StandardBacktracking,
-                 policy: Callable[[CSPNode, int], bool] = CSPPolicy.InsertOrder
+    def __init__(self, algorithm: Callable[[CSPNode, CSPConstraints, int], bool] = CSPAlgorithm.StandardBacktracking,
+                 policy: Callable[[CSPNode, int], bool] = CSPPolicy.InsertOrder,
                  step_arc_consistency: bool = False):
-        self._constraints: Dict[Tuple, Callable]
+        self._constraints: CSPConstraints
         self._root = CSPNode(None, [])
         self._solutions: List[CSPNode] = []
         self._found_solution = False
@@ -26,13 +24,8 @@ class CSPSolver:
         for v in variables:
             if not self._root.get_variable_by_name(v.name):
                 raise ValueError("The variable " + v + " is not a variable in this problem.")
-
-        if variables_len == 1:
-            self.constraint[variables] = constraint
-        elif variables_len == 2:
-            # Two versions of the same constraint to make search faster
-            self.constraint[(variables[0],variables[1])] = constraint
-            self.constraint[(variables[1],variables[0])] = constraint
+        
+        self._constraints.add_constraint(varibles, constraint)
 
     # Add a variable
     def add_variable(self, name: str, domain: List):
@@ -47,15 +40,15 @@ class CSPSolver:
     def show_constraints(self):
         pass
 
-    def _next_step(self, current_node: CSPNode, tree_depth: int):
+    def _next_step(self, current_node: CSPNode, tree_depth: int, one_solution: bool):
         # assegno la variabile e faccio uno snapshot delle variabili
         # in questo modo se quella assegnazione fallisce, posso tornare allo snapshot
-        variable = self._choose_variable_to_assign(self._policy, current_node, tree_depth)        
+        variable = self._choose_variable_to_assign(self._policy, current_node, tree_depth)
         
         # ciclo sui valori della variabile da assegnare
         for value in variable.domain:
             # Costruiamo il prossimo nodo
-            child_node = CSPNode(current_node, current_node.get_variables_list) # TODO: Il costruttore vuole una lista di variabili, sistema!
+            child_node = CSPNode(current_node, current_node.get_variables_list[:]) # TODO: copia della lista di variabili
             current_node.add_child(child_node)
 
             # Assegna un valore alla variabile da assegnare
@@ -63,8 +56,10 @@ class CSPSolver:
 
             # Algoritmo applicato al child node: i domini delle variabili non assegnate verranno modificati (a seconda dell'algoritmo scelto)
             if self.alg(child_node, tree_depth):
-                if vuoi_applicare_consistenza_ad_ogni_passo:
-                    applica_consistenza
+                if self._step_arc_consistency:
+                    if not self.apply_arc_consistency(child_node):
+                        child_node.set_failure()
+                        continue
                 
                 if tree_depth + 1 == len(child_node.get_variables_list()):
                     child_node.set_solution()
@@ -82,8 +77,8 @@ class CSPSolver:
                 # Descrizione del fallimento
                 child_node.set_failure()
     
-    def solve(self):
-        self._next_step(self._root, 0)
+    def solve(self, one_solution: bool = True):
+        self._next_step(self._root, 0, one_solution)
         
     
     def apply_arc_consistency(self, node: CSPNode):
@@ -91,6 +86,23 @@ class CSPSolver:
 
     def apply_node_consistency(self, node: CSPNode):
         return True
+        
+
+class CSPConstraints:
+    def __init__(self, ):
+        self._contraints : Dict[Tuple[str, ...], List[Callable]]
+
+    def add_constraint(self,variables: Tuple[str, ...], constraint: Callable):
+        if not self.constraint[variables]:
+            self.constraint[variables] = []
+
+        self.constraint[variables].append(constraint)
+    
+    def get_unary_constraints(self, a):
+        return dict[(a)]
+
+    def get_binary_constraints(self, a, b):
+        return (dict[(a, b)], dict[(b, a)])
 
 
 class CSPVariable:
@@ -101,7 +113,6 @@ class CSPVariable:
     
     def assign_value(self, value):
         self.value = value
-
 
 class CSPNode:
     def __init__(self, parent: CSPNode, variables: List[CSPVariable]):
@@ -144,11 +155,28 @@ class CSPNode:
 
 class CSPAlgorithm:
     @staticmethod
-    def GenerateAndTest(node: CSPNode, tree_depth: int):
+    def GenerateAndTest(node: CSPNode, constraints: CSPConstraints, tree_depth: int):
         return True
 
     @staticmethod
-    def StandardBacktracking(node: CSPNode, tree_depth: int):
+    def StandardBacktracking(node: CSPNode, constraints: CSPConstraints, tree_depth: int, last_assigned_variable_name: str):
+        variable = node.get_variable_by_name(last_assigned_variable_name)
+        # verifico vincoli unari
+        for constraint in constraints.get_unary_constraints(variable.name):
+            if not constraint(variable.value):
+                return False
+        
+        # verifico vincoli binari
+        # prendo variabili assegnate diverse da quella appena assegnata
+        for assigned_var in [var in node.get_variables_list if var.value and not variable.name == var.name]:
+            cs_1, cs_2 = constraints.get_binary_constraints(variable.name, assigned_var.name)
+            for constraint in cs_1:
+                if not constraint(variable.value, assigned_var.value):
+                    return False
+            for constraint in cs_2:
+                if not constraint(assigned_var.value, variable.value):
+                    return False
+
         return True
 
     @staticmethod
